229f251526454cc3b22f692d842495e09c2c5f6e
profithesame <profi.net.md@gmail.com>
2024-09-24 17:29:47 +0300

    BLOG: do not save comments

--- profithesame/blog/views.py
+++ profithesame/blog/views.py
@@ -26,9 +26,9 @@ def post_detail(request, year, month, day, post):
         # EDIT let only registred users to make comment
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
+            # new_comment = comment_form.save(commit=False)
+            # new_comment.post = post
+            # new_comment.save()
     else:
         comment_form = CommentForm()
 
