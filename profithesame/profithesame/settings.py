4b9f9cf52e1133b4058dde8b987ad1209cdeb44c
profithesame <profi.net.md@gmail.com>
2024-09-24 15:00:34 +0300

    BLOG: add static

--- profithesame/profithesame/settings.py
+++ profithesame/profithesame/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .secret_key import SECRET_KEY
 
@@ -115,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
