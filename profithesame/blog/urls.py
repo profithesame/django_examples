6dfcf585c086c891de7360dfbc210e5ad8661d11
profithesame <profi.net.md@gmail.com>
2024-09-24 13:00:27 +0300

    BLOG: Change paginator to native one

--- profithesame/blog/urls.py
+++ profithesame/blog/urls.py
@@ -1,12 +1,11 @@
-from django.urls import re_path, include
+from django.urls import re_path
 from . import views
 
 
 app_name = 'blog'
 urlpatterns = [
     # post views
-    # re_path(r'^$', views.post_list, name='post_list'),
-    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^$', views.PostListView.as_view(), name='post_list'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
         r'(?P<post>[-\w]+)/$',
         views.post_detail,
