55b3ad373b547f759ee1f54483357e6d3d18f70c
profithesame <profi.net.md@gmail.com>
2024-09-25 15:24:25 +0300

    BLOG: add sitemap

--- profithesame/profithesame/urls.py
+++ profithesame/profithesame/urls.py
@@ -16,9 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
-#from django.conf.urls import include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^blog/', include('blog.urls'))
+    re_path(r'^blog/', include('blog.urls')),
+    re_path(
+        r'^sitemap\.xml$',
+        sitemap,
+        {'sitemaps':sitemaps},
+        name='django.contrib.sitemaps.views.sitemaps'),
 ]
