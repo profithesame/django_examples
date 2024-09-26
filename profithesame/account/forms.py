79138c14a4a658f7d0cc9ca01476e8749b4c4a2f
profithesame <profi.net.md@gmail.com>
2024-09-26 15:00:18 +0300

    ACC: add register user

--- profithesame/account/forms.py
+++ profithesame/account/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth.models import User
+
+from typing import Dict, Any
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+        widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean(self) -> Dict[str, Any]:
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:  
+            raise forms.ValidationError('Passwords don\'t match.')
+        return super().clean()
--- profithesame/account/views.py
+++ profithesame/account/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
+from .forms import UserRegistrationForm
+
 
 @login_required
 def dashboard(request:HttpRequest):
@@ -21,4 +23,31 @@ def user_logout(request:HttpRequest):
         request,
         'registration/logged_out.html',
         {}
+    )
+
+def register(request:HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+
+            return render(request,
+                'account/register_done.html',
+                {
+                    'new_user': new_user
+                }
+            )
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(
+        request,
+        'account/register.html',
+        {
+            'user_form': user_form
+        }
     )
