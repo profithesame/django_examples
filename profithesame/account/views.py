73d445d09ac6ebe9bc14e72cc01ae194885ed9cf
profithesame <profi.net.md@gmail.com>
2024-09-26 18:35:13 +0300

    ACC: addd message on saving profile

--- profithesame/account/views.py
+++ profithesame/account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
@@ -74,6 +75,15 @@ def edit(request:HttpRequest) -> HttpResponse:
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully'
+            )
+        else:
+            messages.error(
+                request,
+                'Error updating your profile'
+            )
 
     else:
         user_form = UserEditForm(instance=request.user)
