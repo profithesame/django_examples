0637032dc059bcea532fb7238ec84f461e6a95a4
profithesame <profi.net.md@gmail.com>
2024-09-30 16:18:37 +0300

    ACC: add create profile for social auth

--- profithesame/account/authentication.py
+++ profithesame/account/authentication.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 
-
+from account.models import Profile
 
 class EmailAuthBackend:
     """
@@ -25,3 +25,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for social authentication
+    """
+    Profile.objects.get_or_create(user=user)
+    # EDIT add email to user
--- profithesame/profithesame/settings.py
+++ profithesame/profithesame/settings.py
@@ -25,6 +25,19 @@ from .smtp_settings import (
     EMAIL_USE_TLS,
 )
 
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 AUTHENTICATION_BACKENDS = [
