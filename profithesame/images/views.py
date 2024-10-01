44e3bbd54e9061298bc4452a15112b56c84f0cac
profithesame <profi.net.md@gmail.com>
2024-10-01 17:27:16 +0300

    SHARE: add share bookmarklet

---
+++ profithesame/images/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .forms import ImageCreateForm
+from .models import Image
+
+
+@login_required
+def image_create(request:HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+
+            messages.success(request, 'Image added successfully')
+
+            return redirect(new_image.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+
+    return render(
+        request,
+        'images/image/create.html',
+        {
+            'section': 'images',
+            'form': form,
+        }
+    )
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        'images/image/detail.html',
+        {
+            'section': 'images',
+            'image': image
+        }
+    )
