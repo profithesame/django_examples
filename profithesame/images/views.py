29517da49ca1a49fde87e47bacbfd257c5524f1c
profithesame <profi.net.md@gmail.com>
2024-10-01 18:25:39 +0300

    SHARE: add like to image and ajax list

--- profithesame/images/views.py
+++ profithesame/images/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -42,5 +44,60 @@ def image_detail(request, id, slug):
         {
             'section': 'images',
             'image': image
+        }
+    )
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    
+    return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request:HttpRequest) -> HttpResponse:
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+        
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {
+                'section': 'images',
+                'images': images
+            }
+        )
+
+    return render(
+        request,
+        'images/image/list.html',
+        {
+            'section': 'images',
+            'images': images
         }
     )
