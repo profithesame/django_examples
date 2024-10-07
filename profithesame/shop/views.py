b34da269a64f24b090010a964a0c821560fb5f98
profithesame <profi.net.md@gmail.com>
2024-10-07 16:28:49 +0300

    SHOP: fix cart_product_form and get_total_price

--- profithesame/shop/views.py
+++ profithesame/shop/views.py
@@ -34,13 +34,13 @@ def product_detail(request:HttpRequest, id, slug) -> HttpResponse:
         slug=slug,
         available=True,
     )
-    cart_product_from = CartAddProductForm
+    cart_product_form = CartAddProductForm
 
     return render(
         request,
         'shop/product/detail.html',
         {
             'product': product,
-            'cart_product_from': cart_product_from,
+            'cart_product_form': cart_product_form,
         }
     )    
