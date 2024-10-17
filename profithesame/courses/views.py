964f5b7483b221db8ff20158f2c84acc053f3a70
profithesame <profi.net.md@gmail.com>
2024-10-17 12:47:18 +0300

    COURSES: using formsets for course module

--- profithesame/courses/views.py
+++ profithesame/courses/views.py
@@ -1,6 +1,8 @@
 from typing import Any
 
 from django.urls import reverse_lazy
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import (
     CreateView,
@@ -11,10 +13,42 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )
+from django.views.generic.base import TemplateResponseMixin, View
 from django.db.models import QuerySet
 
 from .models import Course
+from .forms import ModuleFormSet
 
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+            data=data)
+        
+    def dispatch(self, request: HttpRequest, pk: int) -> HttpResponse:
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset,
+        })
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset,
+        })
 
 class OwnerMixin:
     def get_queryset(self) -> QuerySet[Any]:
