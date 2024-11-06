302579c57568b0a5bd351c2c6d019fcab7d2d77d
profithesame <profi.net.md@gmail.com>
2024-11-06 13:45:45 +0200

    CHAT: add context to the messages

--- profithesame/chat/consumers.py
+++ profithesame/chat/consumers.py
@@ -1,11 +1,13 @@
 import json
 from asgiref.sync import async_to_sync
-
 from channels.generic.websocket import WebsocketConsumer
+
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
         # join room group
@@ -26,6 +28,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
         # send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -33,6 +36,8 @@ class ChatConsumer(WebsocketConsumer):
             {
                 'type': 'chat_message',
                 'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
